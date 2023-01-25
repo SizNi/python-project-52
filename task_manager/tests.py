5192a3eee2642936618bb4de2b974bdcdc3e2158
SizNi <wwwwwwwww@list.ru>
2023-01-25 23:48:55 +0300

    typo

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.utils.translation import gettext_lazy as _
 
+
 class TestHomePage(TestCase):
-    
+
     def test_open(self):
         resp = self.client.get('')
         self.assertEqual(resp.status_code, 200)
-    
+
     def test_context(self):
         resp = self.client.get('')
         self.assertContains(resp, _('Менеджер задач'), status_code=200)
