efe036ed46d08b92e8e6817885dd222dcc8326bc
SizNi <wwwwwwwww@list.ru>
2023-01-26 00:32:27 +0300

    add csrf trusted

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -125,6 +125,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://python-project-52-production-e016.up.railway.app/'
+]
+
 LOGIN_URL = 'login'
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
