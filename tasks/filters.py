3dd033073d9b91491affe8c059045fcf70fed8d1
SizNi <wwwwwwwww@list.ru>
2023-01-27 02:13:06 +0300

    typo

--- tasks/filters.py
+++ tasks/filters.py
@@ -6,7 +6,7 @@ from labels.models import Labels
 
 
 class TasksFilterForm(FilterSet):
-    labels = ModelChoiceFilter(label=_('Label'),
+    labels = ModelChoiceFilter(label=_('Метка'),
                             queryset=Labels.objects.all())
     only_user = BooleanFilter(
         label=_('Только свои задачи'),
